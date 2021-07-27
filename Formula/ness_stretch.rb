516e7d91a7f4ed3ac6ec15adcba17c98d8b3b8bb
Sam <spluta@gmail.com>
2021-07-27 18:22:16 +0200

    9 layer multithreading rust

--- Formula/ness_stretch.rb
+++ Formula/ness_stretch.rb
@@ -3,10 +3,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class NessStretch < Formula
   desc "NessStretch time stretching algorithm written in Rust"
-  homepage "https://github.com/spluta/ness_stretch"
-  url "https://github.com/spluta/TimeStretch/releases/download/0.2.0/ness_stretch-mac.tar.gz"
-  sha256 "46d00499137224fe9fa8f0f3d022e1fdd31f4b546634566e056a9ae73fedc1aa"
-  version "0.1.1"
+  homepage "https://github.com/spluta/TimeStretch"
+  url "https://github.com/spluta/TimeStretch/releases/download/0.3.0/ness_stretch-mac.tar.gz"
+  sha256 "51902763e194693c41cf731e1d41251a0b760091ea41f7ad4110445046c4ef20"
+  version "0.3.0"
 
   def install
     bin.install "ness_stretch"
