f93094e512cd409f2c79e28531d329d7adaa0aaa
Sam <spluta@gmail.com>
2021-07-29 13:41:51 +0200

    update to memory efficient model

--- Formula/ness_stretch.rb
+++ Formula/ness_stretch.rb
@@ -5,7 +5,7 @@ class NessStretch < Formula
   desc "NessStretch time stretching algorithm written in Rust"
   homepage "https://github.com/spluta/TimeStretch"
   url "https://github.com/spluta/TimeStretch/releases/download/0.3.0/ness_stretch-mac.tar.gz"
-  sha256 "36082595e24fa794c3c10b1be63da959e9c223b910802b3500cd2090c4ee628f"
+  sha256 "4d4dadbaa860bb7cfc4ebf41bae0d72af532e9f83f2f50525ef8c812534e23e1"
   version "0.3.0"
 
   def install
