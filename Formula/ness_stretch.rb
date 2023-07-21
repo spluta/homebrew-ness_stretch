1274e4d46f07379afa92129a6825e306436b081d
spluta <spluta@gmail.com>
2023-07-21 18:09:46 -0400

    Update ness_stretch.rb

--- Formula/ness_stretch.rb
+++ Formula/ness_stretch.rb
@@ -4,8 +4,8 @@
 class NessStretch < Formula
   desc "NessStretch time stretching algorithm written in Rust"
   homepage "https://github.com/spluta/TimeStretch"
-  url "https://github.com/spluta/ness_stretch/releases/download/0.2.3/ness_stretch-arm64.tar.gz"
-  sha256 "96f602505dd6e3d3dafce02fd1e3484ed8a47dd8a4403d0eec0f0e8d42c25ce3"
+  url "https://github.com/spluta/ness_stretch/releases/download/0.2.3/ness_stretch_universal.tar.gz"
+  sha256 "60b48d03bf5c3c5da87089164d20a15fcb50af4bfc40988223abc93e4e68c961"
   version "0.4.3"
 
   def install
